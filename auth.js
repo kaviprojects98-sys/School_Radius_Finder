c02d630dfd2d48fd5bd0d999911d87203a1825d3
kaviprojects98-sys <kaviprojects98@gmail.com>
2026-07-19 21:01:45 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
