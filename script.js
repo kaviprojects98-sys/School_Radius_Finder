0647d4674f285dbdf1b99ee9e51474a4f09b6289
kaviprojects98-sys <kaviprojects98@gmail.com>
2026-07-19 20:02:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+import { getAuth, onAuthStateChanged }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const auth = getAuth();
+
+
+onAuthStateChanged(auth,(user)=>{
+
+if(!user){
+
+window.location.href="login.html";
+
+}
+
+});
+
+
 let map;
 let studentMarker;
 let schoolMarker;
