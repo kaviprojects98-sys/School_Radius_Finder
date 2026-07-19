e589ac83853556e0e6a5c92ab225d4700c2fd047
kaviprojects98-sys <kaviprojects98@gmail.com>
2026-07-19 21:03:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./auth.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDPkiY9WV152Sa6AOkx4ySISWNhYoIH0c0",
-    authDomain: "schoolradiusfinder-b7597.firebaseapp.com",
-    projectId: "schoolradiusfinder-b7597",
-    storageBucket: "schoolradiusfinder-b7597.firebasestorage.app",
-    messagingSenderId: "1049648379138",
-    appId: "1:1049648379138:web:d756f1688264b30d3e3e1b",
-    measurementId: "G-V69ZJT16YL"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
-    if (!user) {
-        window.location.href = "login.html";
-    }
+    if (!user) window.location.href = "login.html";
 });
 
 let map;
